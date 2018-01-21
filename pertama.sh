3f769bfbab838b6fc3dab3aa899396b5fa0085ac
agengsetyo <09011281419041@students.ilkom.unsri.ac.id>
2018-01-21 23:35:13 +0700

    Delete pertama.sh

--- pertama.sh
+++
@@ -1,5 +0,0 @@
-sudo yum install git -y 
-sudo yum install centos-release-scl epel-release -y 
-sudo yum install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make -y 
-scl enable devtoolset-4 bash 
-git clone https://github.com/fireice-uk/xmr-stak.git
