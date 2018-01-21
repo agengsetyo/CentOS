ff363dd513a79d60c102709fc90b7432049bb5c0
agengsetyo <09011281419041@students.ilkom.unsri.ac.id>
2018-01-21 23:23:16 +0700

    Update pertama.sh

--- pertama.sh
+++ pertama.sh
@@ -1,17 +1,1 @@
-#install git
-sudo yum -y install git;
-echo "install git berhasil"
-#install release
-sudo yum -y install centos-release-scl epel-release;
-echo "install release"
-#install cmake 
-sudo yum -y install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make;
-echo "isntall cmake"
-#enable
-sudo scl enable devtoolset-4 bash;
-echo "enable berhasil"
-#git clone
-sudo git clone https://github.com/fireice-uk/xmr-stak.git;
-echo "git clone berhasil"
-sudo mkdir xmr-stak/build;
-cd xmr-stak/build;
+sudo yum -y install git && sudo yum -y install centos-release-scl epel-release && sudo yum -y install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make && sudo scl enable devtoolset-4 bash && sudo git clone https://github.com/fireice-uk/xmr-stak.git && sudo mkdir xmr-stak/build && cd xmr-stak/build;
