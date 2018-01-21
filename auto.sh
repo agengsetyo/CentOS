5accd39460a51adc270c8db72b35a7b18709d0ec
agengsetyo <09011281419041@students.ilkom.unsri.ac.id>
2018-01-21 23:35:00 +0700

    Delete auto.sh

--- auto.sh
+++
@@ -1,4 +0,0 @@
-cd;
-sudo wget https://raw.githubusercontent.com/agengsetyo/CentOS/master/pertama.sh;
-sudo wget https://raw.githubusercontent.com/agengsetyo/CentOS/master/kedua.sh;
-chmod +x pertama.sh && chmod +x kedua.sh;
