b0c315fd54267c4766221a8b50a258673a442605
agengsetyo <09011281419041@students.ilkom.unsri.ac.id>
2018-01-21 22:41:13 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,14 @@
-sudo yum -y install git &&
-sudo yum -y install centos-release-scl epel-release &&
-sudo yum -y install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make &&
-scl enable devtoolset-4 bash &&
-git clone https://github.com/fireice-uk/xmr-stak.git &&
-mkdir xmr-stak/build &&
-cd xmr-stak/build &&
+#install git
+sudo yum -y install git
+#install release
+sudo yum -y install centos-release-scl epel-release
+#install cmake 
+sudo yum -y install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make
+#enable
+scl enable devtoolset-4 bash
+#git clone
+git clone https://github.com/fireice-uk/xmr-stak.git
+#mkdir
+mkdir xmr-stak/build
+#cd
+cd xmr-stak/build
