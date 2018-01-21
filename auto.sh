74ebd69e25a6cc1c725b69a3c8f924c28dd9aaf0
agengsetyo <09011281419041@students.ilkom.unsri.ac.id>
2018-01-21 23:05:53 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,1 +1,6 @@
+cd
 
+sudo wget https://raw.githubusercontent.com/agengsetyo/CentOS/master/pertama.sh
+sudo wget https://raw.githubusercontent.com/agengsetyo/CentOS/master/kedua.sh
+
+chmod +x pertama.sh && chmod +x kedua.sh
